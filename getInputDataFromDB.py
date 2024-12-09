1aec71d2c32f313f62bfee24b8f154c3a8d08f47
Shamil Otsokov <shamil24@mail.ru>
2024-12-09 12:48:24 +0300

    Добавили чтобы в файл исходных данных добавлялся option_id и page_id

--- getInputDataFromDB.py
+++ getInputDataFromDB.py
@@ -24,7 +24,7 @@ def get_kafedra_list(server, database, username, password):
 # Функция для получения данных для каждой кафедры
 def get_kafedra_data(server, database, username, password, kafedra):
     sql_query = f"""
-        SELECT page, question_description, question_id, option_name, answer
+        SELECT page, page_id, question_description, question_id, option_id,  option_name, answer
         FROM [dbo].[EFQM_SurveyAnswers]
         WHERE [SubdivisionShortName] = ?
     """
@@ -39,10 +39,12 @@ def get_kafedra_data(server, database, username, password, kafedra):
         for row in rows:
             kafedra_data.append({
                 "page": row[0],
-                "question_description": row[1],
-                "question_id": row[2],
-                "option_name": row[3],
-                "answer": row[4]
+                "page_id": row[1],
+                "question_description": row[2],
+                "question_id": row[3],
+                "option_id": row[4],
+                "option_name": row[5],
+                "answer": row[6]
             })
         
         cursor.close()
@@ -52,9 +54,13 @@ def get_kafedra_data(server, database, username, password, kafedra):
     
     return kafedra_data
 
+
+    
 # Функция для преобразования данных в иерархический JSON формат
 def transform_to_hierarchical_format(kafedra_name, kafedra_data):
-    hierarchical_data = {
+    hierarchical_data = []
+    
+    kafedra_entry = {
         "SubdivisionShortName": kafedra_name,
         "pages": []
     }
@@ -62,34 +68,44 @@ def transform_to_hierarchical_format(kafedra_name, kafedra_data):
     pages = {}
     for item in kafedra_data:
         page = item["page"]
+        page_id = item["page_id"]
         question_description = item["question_description"]
         question_id = item["question_id"]
+        option_id = item["option_id"]
         option_name = item["option_name"]
         answer = item["answer"]
         
         if page not in pages:
-            pages[page] = {}
+            pages[page] = {
+                "page_id": page_id,
+                "questions": {}
+            }
         
-        if question_id not in pages[page]:
-            pages[page][question_id] = {
+        if question_id not in pages[page]["questions"]:
+            pages[page]["questions"][question_id] = {
                 "question_description": question_description,
                 "question_id": question_id,
                 "options": []
             }
         
-        pages[page][question_id]["options"].append({
+        pages[page]["questions"][question_id]["options"].append({
+            "option_id":option_id,
             "option_name": option_name,
             "answer": answer
         })
     
-    for page_name, questions in pages.items():
+    for page_name, page_content in pages.items():
         page_data = {
             "page": page_name,
-            "questions": list(questions.values())
+            "page_id": page_content["page_id"],
+            "questions": list(page_content["questions"].values())
         }
-        hierarchical_data["pages"].append(page_data)
+        kafedra_entry["pages"].append(page_data)
+    
+    hierarchical_data.append(kafedra_entry)
     
     return hierarchical_data
+
 
 # Основная программа
 def main():
